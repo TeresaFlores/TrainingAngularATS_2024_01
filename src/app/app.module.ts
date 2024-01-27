1578732a7570545cfba9bf6a5b69b84e56777576
Hector Velazquez <hector.a.velazquez@accenture.com>
2024-01-26 22:43:20 -0500

    Generated initial components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DynamicFormComponent } from './components/shared/dynamic-form/dynamic-form.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { WrapperComponent } from './components/shared/ui/wrapper/wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DynamicFormComponent,
+    NavigationComponent,
+    SidebarComponent,
+    ModalComponent,
+    NavbarComponent,
+    WrapperComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/shared/dynamic-form/dynamic-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-form',
+  templateUrl: './dynamic-form.component.html',
+  styleUrls: ['./dynamic-form.component.css']
+})
+export class DynamicFormComponent {
+
+}
