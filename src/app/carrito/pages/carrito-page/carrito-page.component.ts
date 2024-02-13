f678ccc2ccd063a39751dabf55fc195041dc486c
Adalberto Zanabria <adalberto.zanabria@accenture.com>
2024-02-13 03:35:10 -0600

    Terminada lógica de carrito de compras.

--- src/app/carrito/pages/carrito-page/carrito-page.component.ts
+++ src/app/carrito/pages/carrito-page/carrito-page.component.ts
@@ -7,16 +7,16 @@ import { CarritoService } from '../../services/carrito.service';
   styleUrls: ['./carrito-page.component.css']
 })
 export class CarritoPageComponent {
-
+  public tasaCambio: number = 1;
+  public moneda: "MXN" | "USD" | "EUR" = "MXN";
   public carrito: any[] = [];
-  // public totalCarrito: number = 0;
+  public compraRealizada: boolean = false;
 
   get totalCarrito() {
-    return this.carrito.reduce((total, item) => total + item.precio, 0);
+    return this.carrito.reduce((total, item) => total + item.precio, 0) * this.tasaCambio;
   }
 
   constructor(private carritoService: CarritoService) {
     this.carrito = this.carritoService.obtenerCarrito();
-    
   }
 }
